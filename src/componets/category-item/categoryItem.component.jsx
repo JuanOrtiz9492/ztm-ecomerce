1cbdc1c1375b47731889934904490c8aea54af1b
Juan Ortiz <juandavid.ortiz@globant.com>
2022-05-02 22:29:45 -0500

    added directory

---
+++ src/componets/category-item/categoryItem.component.jsx
@@ -0,0 +1,15 @@
+import './categories.styles.scss'
+
+const CategoryItem = ({ imageUrl, title }) => (
+    <div className='category-container'>
+        <div className='background-image' style={{
+            backgroundImage: `url(${imageUrl})`
+        }} />
+        <div className='category-body-container'>
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+        </div>
+    </div>
+)
+
+export default CategoryItem
