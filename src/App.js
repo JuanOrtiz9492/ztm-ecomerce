d6f9d0d61a52a700cde869253043264275041421
Juan Ortiz <juandavid.ortiz@globant.com>
2022-05-04 13:21:20 -0500

    authentication view

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Home from "./routes/home/home.component";
 import Dummy from "./componets/dummy/dummy";
-import Signin from "./routes/singin/singin.component";
+import Authentication from "./routes/authentication/authentication.component";
 import Navigation from "./routes/navigation/navigation.component";
 import { Routes, Route } from "react-router-dom";
 
@@ -9,7 +9,7 @@ const App = () => (
         <Route path="/" element={<Navigation />}>
             <Route index element={<Home />} />
             <Route path="/shop" element={<Dummy viewName="Shop" />} />
-            <Route path="sign-in" element={<Signin />} />
+            <Route path="auth" element={<Authentication />} />
         </Route>
     </Routes>
 )
